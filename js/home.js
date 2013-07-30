45ceec0f3ab2a65867dbe2d02d24ab3960745434
Nick <nick@nebula.io>
2013-07-29 17:07:17 -1000

    scrollaway header

--- js/home.js
+++ js/home.js
@@ -3,4 +3,27 @@ $(function() {
      $('.workContent div').hide();
      $('.workContent div.' + this.id).show();
    });
+
+
+   var lastScroll = null;
+   var timeoutId = null;
+   function update() {
+     var scrollTop = $(document.body).scrollTop();
+     var headerHeight = $('div.scrollaway').height();
+     var targetPosition = Math.max(-(headerHeight), -scrollTop);
+     $('.site-header').css('top', targetPosition + 'px');
+     timeoutId = null;
+   }
+   $(window).scroll(function() {
+     if (!lastScroll || (Date.now() - lastScroll) > 30) {
+       update();
+       lastScroll = Date.now();
+    } else {
+      if (timeoutId !== null) {
+        clearTimeout(timeoutId);
+      }
+      lastScroll = Date.now();
+      timeoutId = setTimeout(update, 150);
+    }
+   });
 });
